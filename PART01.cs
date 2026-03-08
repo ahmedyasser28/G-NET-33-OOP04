3760130af0d95a961da7833a188698d10c80d33e
Ahmed Yasser <ahmedyasser199.aya@gmail.com>
2026-03-08 05:39:27 +0200

    Add files via upload

---
+++ PART01.cs
@@ -0,0 +1,52 @@
+﻿/*
+==========================
+Part 01 – Theoretical
+==========================
+
+Q1: Static Binding vs Dynamic Binding
+
+Static Binding:
+- Happens at compile time.
+- Method call is determined during compilation.
+- Used in method overloading.
+- Faster because it does not depend on runtime type.
+
+Dynamic Binding:
+- Happens at runtime.
+- Method call depends on the object type.
+- Used in method overriding (polymorphism).
+- Requires inheritance and virtual methods.
+
+=====================================================
+
+Q2: Method Overloading vs Method Overriding
+
+Method Overloading:
+- Same method name.
+- Different parameters.
+- Happens in same class.
+- Compile-time (static binding).
+
+Method Overriding:
+- Same method name and same parameters.
+- Happens in parent and child classes.
+- Requires inheritance.
+- Runtime (dynamic binding).
+
+
+=============================================
+
+Q3: Keywords used for Method Overriding
+
+virtual:
+- Used in base class.
+- Allows method to be overridden.
+
+override:
+- Used in child class.
+- Replaces base class implementation.
+
+base:
+- Used to call base class version of method.
+
+*/
